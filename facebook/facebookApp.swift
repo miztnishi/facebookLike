f1b7849e8b7e952d5b27290ac18325e805701779
水谷 <mizu710tr@gmail.com>
2021-01-04 22:29:41 +0900

    Initial Commit

---
+++ facebook/facebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebookApp.swift
+//  facebook
+//
+//  Created by first on 2021/01/04.
+//
+
+import SwiftUI
+
+@main
+struct facebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
